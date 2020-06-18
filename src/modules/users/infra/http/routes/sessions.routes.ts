d46d4e88d3fb3b4de7e992e6d2f6d33ab81b42da
Matheus <matheusrdelima@outlook.com>
2020-06-17 22:30:07 -0300

    Injeção de dependências e Usando controllers

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,22 +1,10 @@
 import { Router } from 'express';
 
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import SessionsController from '../controllers/SessionsController';
 
 const sessionsRouter = Router();
+const sessionsController = new SessionsController();
 
-sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
-
-  const usersRepository = new UsersRepository();
-  const authenticateUserService = new AuthenticateUserService(usersRepository);
-
-  const { user, token } = await authenticateUserService.execute({
-    email,
-    password,
-  });
-
-  return response.json({ user, token });
-});
+sessionsRouter.post('/', sessionsController.create);
 
 export default sessionsRouter;
