f131a81d7798f18ad9db06ac49d2df4cb1e4c408
Matheus <matheusrdelima@outlook.com>
2020-09-14 21:34:19 -0300

    Template engine

--- src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,9 +1,8 @@
-import IMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 import IMailTemplateProvider from '../models/IMailTemplateProvider';
 
 class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({ template }: IMailTemplateDTO): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'Mail content';
   }
 }
 
