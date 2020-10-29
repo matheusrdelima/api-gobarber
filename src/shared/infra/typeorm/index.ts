d2a48e7e48f09fb89dc1f510c565f86b3e0b2b8d
Matheus <matheusrdelima@outlook.com>
2020-10-29 19:39:40 -0300

    Estrutura de notificações

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,7 +1,7 @@
-import { createConnection } from 'typeorm';
+import { createConnections } from 'typeorm';
 
 // procura o arquivo ormconfig.json
 // Poderíamos incluir as informações diretamente como parametros do método abaixo,
 // porém a CLI do Typeorm também faz a leitura do arquivo ormconfig.json, ou seja,
 // não há a necessidade de passar as informações aqui já que é feito a leitura também;
-createConnection();
+createConnections();
