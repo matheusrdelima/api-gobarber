9ef3bff41861ddbe961b15d49ca0b6b324ce879c
Matheus <matheusrdelima@outlook.com>
2020-11-17 20:04:44 -0300

    Emails pelo Amazon SES

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'test@test.com.br',
+      name: 'John Doe',
+    },
+  },
+} as IMailConfig;
